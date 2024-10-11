c08003c695bb331150de579c2a29ea7e0204d328
Stephanie Leroux <stephanieleroux@users.noreply.github.com>
2024-10-11 13:47:00 +0200

    Update entrypoint.sh

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -1,10 +1,1 @@
-#!/bin/sh
 
-# Activate the Spack environment
-. /opt/spack-environment/activate.sh  # Source the Spack activation script
-
-# Start Jupyter Lab in the background
-jupyter lab --ip=0.0.0.0 --no-browser --allow-root &
-
-# Execute any additional commands passed to the script
-exec "$@"
